23d5d0255fec3349c3a307b4f2664966250efa1f
akinoai <mochalovaveronika60@gmail.com>
2025-09-30 19:00:46 +0300

    update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -14,7 +14,8 @@ const Nav = () => {
             <li key={item.label}>
               <a
                 href={item.href}
-                className="font-montserrat leading-normal text-lg text-slate-gray"
+                className="font-montserrat leading-normal text-lg 
+                        text-slate-gray hover:text-coral-red transition-colors duration-200"
               >
                 {item.label}
               </a>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,10 @@
 import { arrowRight } from "../assets/icons";
 import Button from "../components/Button";
+import { shoes, statistics } from "../constants/index.js";
+import { bigShoe1 } from "../assets/images/index.js";
+import { bigShoe2 } from "../assets/images/index.js";
+import { bigShoe3 } from "../assets/images/index.js";
+import ShoeCard from "../components/ShoeCard.jsx";
 
 const Hero = () => {
   return (
@@ -8,19 +13,59 @@ const Hero = () => {
       className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-        <p>Our Summer Collection</p>
-        <h1>
-          <span>The New Arrival</span>
+        <p className="text-xl font-montserrat text-coral-red">
+          Our Summer Collection
+        </p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            The New Arrival
+          </span>
           <br />
-          <span>Nike</span>
+          <span className="text-coral-red inline-block mt-3">Nike </span>
           Shoes
         </h1>
-        <p>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
           Discover stylish Nike arrivals quality comfort and innovation for your
           active life
         </p>
 
-        <Button label="Show now" iconURL={arrowRight} />
+        <Button label="Shop now" iconURL={arrowRight} />
+
+        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+          {statistics.map((stat) => (
+            <div key={stat.label}>
+              <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
+              <p className="leading-7 font-montserrat text-slate-gray">
+                {stat.label}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div
+        className="relative flex-1 flex justify-center 
+                      items-center xl:min-h-screen max-xl:py-40 bg-primary bg-cover bg-center"
+      >
+        <img
+          src={bigShoe1}
+          alt="shoe collection"
+          width={610}
+          height={500}
+          className="object-contain relative z-10"
+        />
+
+        <div>
+          {shoes.map((shoe) => (
+            <div key={shoe}>
+              <ShoeCard
+                imgURL={shoe}
+                changeBigShoeImage={() => {}}
+                bigShoeImg=""
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
