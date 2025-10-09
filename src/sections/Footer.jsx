2f0e75403491001442eb602986d4519019ab2e23
akinoai <mochalovaveronika60@gmail.com>
2025-10-09 14:08:38 +0300

    footer section done

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,5 +1,6 @@
+import { copyrightSign } from "../assets/icons";
 import { footerLogo } from "../assets/images";
-import { socialMedia } from "../constants";
+import { footerLinks, socialMedia } from "../constants";
 
 const Footer = () => {
   return (
@@ -24,7 +25,38 @@ const Footer = () => {
           </div>
         </div>
 
-        <div></div>
+        <div className="flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap">
+          {footerLinks.map((section) => (
+            <div key={section}>
+              <h4 className="text-white font-montserrat text-2xl leading-normal font-medium mb-6">
+                {section.title}
+              </h4>
+              <ul>
+                {section.links.map((link) => (
+                  <li
+                    className="mt-3 text-white-400 font-montserrat text-base leading-normal hover:text-slate-gray transition-colors duration-300 cursor-pointer"
+                    key={link.name}
+                  >
+                    <a>{link.name}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center">
+        <div className="flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer">
+          <img
+            src={copyrightSign}
+            width={20}
+            height={20}
+            className="rounded-full m-0"
+          />
+          <p>Copyright. All rights reserved</p>
+        </div>
+        <p className="font-montserrat cursor-pointer">Terms & Conditions</p>
       </div>
     </footer>
   );
