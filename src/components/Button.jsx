7700a4791a698307cf19a922f1d5c30537370bd1
akinoai <mochalovaveronika60@gmail.com>
2025-10-09 11:31:23 +0300

    specialOffer section done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,19 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  textColor,
+  borderColor,
+  fullWidth,
+}) => {
   return (
     <button
-      className="flex justify-center items-center gap-2 px-7 py-4 
-                border font-montserrat text-lg leading-none 
-                bg-coral-red rounded-full text-white border-coral-red"
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-coral-red text-white border-coral-red"
+      } rounded-full ${fullWidth && "w-full"}`}
     >
       {label}
 
@@ -11,7 +21,7 @@ const Button = ({ label, iconURL }) => {
         <img
           src={iconURL}
           alt="arrow right icon"
-          className="ml-2 rounded-full w-5 h-5"
+          className="ml-2 rounded-full bg-white w-5 h-5"
         />
       )}
     </button>
